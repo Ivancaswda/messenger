fd35ed5cc995827e6b2dbc2925bdf867ca064de1
ScienceProject.io <162120681+Ivancaswda@users.noreply.github.com>
2025-03-13 16:26:57 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use('/api/user', userRouter)
 app.use('/api/message', messageRouter)
-app.get('/', (request, response) => response.send('Api работает'))
+
 
 if (process.env.NODE_ENV=== "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
